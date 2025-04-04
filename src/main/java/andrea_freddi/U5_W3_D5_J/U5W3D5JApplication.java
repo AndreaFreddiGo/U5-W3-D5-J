81f371dcd34e22bc10fdf8ff3ad98e96b4d17886
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-04-04 16:22:24 +0200

    Initial commit

---
+++ src/main/java/andrea_freddi/U5_W3_D5_J/U5W3D5JApplication.java
@@ -0,0 +1,13 @@
+package andrea_freddi.U5_W3_D5_J;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D5JApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andrea_freddi/U5_W3_D5_J/U5W3D5JApplicationTests.java
@@ -0,0 +1,13 @@
+package andrea_freddi.U5_W3_D5_J;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
