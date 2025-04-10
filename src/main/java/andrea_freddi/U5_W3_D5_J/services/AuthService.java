78702cf6f07f703b40f5cfbbf3a896820c9b8425
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-04-10 18:33:29 +0200

    Completes AuthService

--- src/main/java/andrea_freddi/U5_W3_D5_J/services/AuthService.java
+++ src/main/java/andrea_freddi/U5_W3_D5_J/services/AuthService.java
@@ -1,7 +1,37 @@
 package andrea_freddi.U5_W3_D5_J.services;
 
+import andrea_freddi.U5_W3_D5_J.entities.User;
+import andrea_freddi.U5_W3_D5_J.exception.UnauthorizedException;
+import andrea_freddi.U5_W3_D5_J.payloads.LoginPayload;
+import andrea_freddi.U5_W3_D5_J.tools.JWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+    // inietto UserService per cercare l'utente
+    @Autowired
+    private UsersService usersService;
+    // inietto JWT per generare il token
+    @Autowired
+    private JWT jwt;
+    // inietto PasswordEncoder per confrontare le password
+    @Autowired
+    private PasswordEncoder bcrypt;
+
+    // creo un metodo per controllare le credenziali e generare il token
+    public String checkCredentialsAndGenerateToken(LoginPayload body) {
+        // cerco se nel database esiste un utente con l'email fornita
+        User found = this.usersService.findByEmail(body.email());
+        // se esiste, confronto la password fornita con quella salvata nel database
+        // e se ok, genero il token
+        if (bcrypt.matches(body.password(), found.getPassword())) {
+            String token = jwt.createToken(found);
+            return token;
+        } else {
+            // se invece la password non corrisponde, lancio un'eccezione
+            throw new UnauthorizedException("Wrong password!");
+        }
+    }
 }
